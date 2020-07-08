eaced89fc559bbeccc867f1e1d684de95fffe1cc
Tim Tissari Bjornstedt <timtissaribjornstedt@gmail.com>
2020-07-08 11:54:18 +0100

    Add logic to populate worksheet
    Javascript to generate random addition and subtraction problems for
    children.

--- randomizers/static/randomizers/script.js
+++ randomizers/static/randomizers/script.js
@@ -1,6 +1,19 @@
-var randomize = document.querySelector('#randomize');
-var sheet = document.querySelector('.sheet');
+const randomize = document.querySelector('#randomize');
+const cols = document.querySelectorAll('.col-body');
+var flag = true;
 
-randomize.addEventListener('click',function(){
-  sheet.textContent = Math.random(0, 10);
-})
+function populate() {
+  cols.forEach(function(col) {
+    num1 = Math.floor(Math.random() * Math.floor(11));
+    num2 = Math.floor(Math.random() * Math.floor(11));
+    if (flag) {
+      col.innerHTML = num1 + " + " + num2 + " = _";
+    } else {
+      col.innerHTML = Math.max(num1, num2) + " - " + Math.min(num1, num2) + " = _";
+    }
+    flag = !flag;
+  });
+};
+
+window.addEventListener('load', populate);
+randomize.addEventListener('click', populate);
