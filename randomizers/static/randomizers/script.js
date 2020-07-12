08327407d08a9b9c0259766b895d98c799b0c5f0
Tim Tissari Bjornstedt <timtissaribjornstedt@gmail.com>
2020-07-12 18:00:46 +0100

    Optimise population script for worksheets
    The script will no longer generate multiple equal maths problems.

--- randomizers/static/randomizers/script.js
+++ randomizers/static/randomizers/script.js
@@ -1,19 +1,37 @@
 const randomize = document.querySelector('#randomize');
-const cols = document.querySelectorAll('.col-body');
-var flag = true;
+const colsLeft = document.querySelectorAll('.col-left');
+const colsRight = document.querySelectorAll('.col-right');
+
 
 function populate() {
-  cols.forEach(function(col) {
-    num1 = Math.floor(Math.random() * Math.floor(11));
-    num2 = Math.floor(Math.random() * Math.floor(11));
-    if (flag) {
-      col.innerHTML = num1 + " + " + num2 + " = _";
-    } else {
-      col.innerHTML = Math.max(num1, num2) + " - " + Math.min(num1, num2) + " = _";
+  numsLeft = [];
+  numsRight = [];
+  colsLeft.forEach(function(col, index) {
+    while (true) {
+      num1 = Math.floor(Math.random() * Math.floor(11));
+      num2 = Math.floor(Math.random() * Math.floor(11));
+      nums = "" + num1 + num2;
+      if (!numsLeft.includes(nums)) {
+        numsLeft.push(nums);
+        col.innerHTML = num1 + " + " + num2 + " = _";
+        break;
+      }
     }
-    flag = !flag;
   });
-};
+  colsRight.forEach(function(col, index) {
+    while (true) {
+      num1 = Math.floor(Math.random() * Math.floor(11));
+      num2 = Math.floor(Math.random() * Math.floor(11));
+      nums = "" + Math.max(num1, num2) + Math.min(num1, num2);
+      if (!numsRight.includes(nums)) {
+        numsRight.push(nums);
+        col.innerHTML = Math.max(num1, num2) + " - " + Math.min(num1, num2) + " = _";
+        break;
+      }
+    }
+  });
+}
+
 
 window.addEventListener('load', populate);
 randomize.addEventListener('click', populate);
